6b5aa350775c62000fb8ddb3c80d311d2aad600d
Luis Ardila <leardilap@unal.edu.co>
2015-09-10 00:10:10 +0200

    03092015: signal tap to look at the FIR mem reading good, but cant read from s1 through pcie

--- linux_app/dcc.c
+++ linux_app/dcc.c
@@ -8,10 +8,14 @@
 #define PCIE_USER_BAR				PCIE_BAR0
 #define PCIE_IO_LED_ADDR			0x00
 #define PCIE_IO_SWITCH_ADDR		0xA0
+#define PCIE_FIR_MEM_ADDR			0x20000
+
+#define FIR_MEM_SIZE					(4*512) // 2048 Bytes
 
 typedef enum{
 	MENU_LED = 0,
 	MENU_SWITCH,
+	MENU_FIR_MEM_WR,
 	MENU_QUIT = 99
 }MENU_ID;
 
@@ -19,6 +23,7 @@ void UI_ShowMenu(void){
 	printf("===TEST===========================\r\n");
 	printf("[%d]: Led control\r\n", MENU_LED);
 	printf("[%d]: Switch Status Read\r\n", MENU_SWITCH);
+	printf("[%d]: FIR Memory Write\r\n", MENU_FIR_MEM_WR);
 	printf("[%d]: Quit\r\n", MENU_QUIT);
 	printf("Please input your selection:");
 }
@@ -34,7 +39,7 @@ BOOL TEST_LED(PCIE_HANDLE hPCIe){
 	BOOL bPass;
 	int	Mask;
 	
-	printf("Please input led conrol mask:");
+	printf("Please input led control mask:");
 	scanf("%d", &Mask);
 
 	bPass = PCIE_Write32(hPCIe, PCIE_USER_BAR, PCIE_IO_LED_ADDR, (DWORD)Mask);
@@ -58,6 +63,73 @@ BOOL TEST_SWITCH(PCIE_HANDLE hPCIe){
 		printf("Failed to read switch status\r\n");
 
 	
+	return bPass;
+}
+
+char PAT_GEN(int nIndex){
+	char Data;
+	Data = nIndex & 0xFF;
+	return Data;
+}
+
+BOOL FIR_MEM_WR(PCIE_HANDLE hPCIe){
+	BOOL bPass=TRUE;
+	int i;
+	const int nTestSize = FIR_MEM_SIZE;
+	const PCIE_LOCAL_ADDRESS LocalAddr = PCIE_FIR_MEM_ADDR;
+	char *pWrite;
+	char *pRead;
+	char szError[256];
+
+
+	pWrite = (char *)malloc(nTestSize);
+	pRead = (char *)malloc(nTestSize);
+	if (!pWrite || !pRead){
+		bPass = FALSE;
+		sprintf(szError, "DMA Memory:malloc failed\r\n");
+	}
+	
+
+	// init test pattern
+	for(i=0;i<nTestSize && bPass;i++)
+		*(pWrite+i) = PAT_GEN(i);
+
+	// write test pattern
+	if (bPass){
+		bPass = PCIE_DmaWrite(hPCIe, LocalAddr, pWrite, nTestSize);
+		if (!bPass)
+			sprintf(szError, "DMA Memory:PCIE_DmaWrite failed\r\n");
+	}		
+
+	// read back test pattern and verify
+	if (bPass){
+		bPass = PCIE_DmaRead(hPCIe, LocalAddr, pRead, nTestSize);
+
+		if (!bPass){
+			sprintf(szError, "DMA Memory:PCIE_DmaRead failed\r\n");
+		}else{
+			for(i=0;i<nTestSize && bPass;i++){
+				if (*(pRead+i) != PAT_GEN(i)){
+					bPass = FALSE;
+					sprintf(szError, "DMA Memory:Read-back verify unmatch, index = %d, read=%xh, expected=%xh\r\n", i, *(pRead+i), PAT_GEN(i));
+				}
+			}
+		}
+	}
+
+
+	// free resource
+	if (pWrite)
+		free(pWrite);
+	if (pRead)
+		free(pRead);
+	
+	if (!bPass)
+		printf("%s", szError);
+	else
+		printf("DMA-Memory (Size = %d byes) pass\r\n", nTestSize);
+
+
 	return bPass;
 }
 
@@ -89,6 +161,9 @@ int main(void)
 					break;
 				case MENU_SWITCH:
 					TEST_SWITCH(hPCIE);
+					break;
+				case MENU_FIR_MEM_WR:
+					FIR_MEM_WR(hPCIE);
 					break;
 				case MENU_QUIT:
 					bQuit = TRUE;
